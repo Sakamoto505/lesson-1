1fc222866c044125ce7a8c1f1da9ae5b397e33d0
Sakamoto505 <name123442@mail.ru>
2022-01-17 20:11:44 +0300

    Added homeworks

---
+++ hw2.rb
@@ -0,0 +1,9 @@
+
+puts "Введите сторону а"
+ a = gets.chomp.to_i
+ 
+ puts "Введите сторону b "
+ h = gets.chomp.to_i
+
+ result = (0.5 * a * h)
+ puts result
---
+++ hw4.rb
@@ -0,0 +1,16 @@
+puts "Введите 3 значения "
+a = gets.chomp.to_f
+b = gets.chomp.to_f
+c = gets.chomp.to_f
+ 
+dis = b * b - 4 * a * c 
+if dis < 0 
+  puts "Нет корней"
+elsif dis == 0 
+x = -b/2 * a
+puts "Есть только один корень #{x}"
+else 
+    x1 = (-b + Math.sqrt(dis))/(2 * a)
+    x2 = (-b - Math.sqrt(dis))/(2 * a)
+    puts "корень 1 #{x1}", "корень 2 #{x2},", "Дис равен #{dis}"
+end
