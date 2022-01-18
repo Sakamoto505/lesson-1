6e469794dfa3d648fc35993527df3a4d390e9d75
Sakamoto505 <name123442@mail.ru>
2022-01-18 13:54:22 +0300

    changed indentation

--- quadratic_equation.rb
+++ quadratic_equation.rb
@@ -4,13 +4,15 @@ b = gets.chomp.to_f
 c = gets.chomp.to_f
  
 dis = b * b - 4 * a * c 
+
 if dis < 0 
   puts "Нет корней"
 elsif dis == 0 
-x = -b/2 * a
-puts "Есть только один корень #{x}"
+  x = -b/2 * a
+
+  puts "Есть только один корень #{x}"
 else 
-    x1 = (-b + Math.sqrt(dis))/(2 * a)
-    x2 = (-b - Math.sqrt(dis))/(2 * a)
-    puts "корень 1 #{x1}", "корень 2 #{x2},", "Дис равен #{dis}"
+  x1 = (-b + Math.sqrt(dis)) / (2 * a)
+  x2 = (-b - Math.sqrt(dis)) / (2 * a)
+  puts "корень 1 #{x1}", "корень 2 #{x2},", "D равен #{dis}"
 end
