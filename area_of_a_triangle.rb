552914886f07254a36320c2d38e2eed018a9fce9
Sakamoto505 <name123442@mail.ru>
2022-01-20 22:24:30 +0300

    preveous lessons

--- area_of_a_triangle.rb
+++
@@ -1,9 +0,0 @@
-
-puts "Введите сторону а"
- a = gets.chomp.to_i
- 
- puts "Введите сторону b "
- h = gets.chomp.to_i
-
- result = (0.5 * a * h)
- puts result
--- hw lesson 2/fibonacci3.rb
+++
@@ -1,7 +0,0 @@
-fib = [0]
-f_num = 1
-while f_num < 100
-  fib << f_num 
-  f_num = fib[-1] + fib[-2]
-end
-  puts fib
